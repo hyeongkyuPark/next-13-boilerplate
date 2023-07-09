2c165fa471e27bb33fafb18b3774b18eb51dcd85
sonic-slogup <phg0644@slogup.com>
2023-07-09 13:52:30 +0900

    done : set storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,47 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  webpackFinal: async (config) => {
+    if (config.resolve?.plugins) {
+      config.resolve?.plugins.push(new TsconfigPathsPlugin({}));
+    } else if (config.resolve) {
+      config.resolve.plugins = [];
+
+      config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+    }
+
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+
+      if (!test) {
+        return false;
+      }
+
+      return test.test('.svg');
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
+export default config;
